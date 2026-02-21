9fea8346a51270283028e5a0477e8b23cf8726bb
Avinash kumar <avinashkumar40977@gmail.com>
2026-02-21 08:36:42 +0530

    [AVINASH] : Added a program of UC1:Feet measurement equality in a Quantity Measurement App

--- src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -1,8 +1,43 @@
 package com.apps.quantitymeasurementapp;
 
-public class QuantityMeasurementApp {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class QuantityMeasurementApp {
+	
+	public static class Feet {
+		private final double feet;
 		
+		public Feet(double feet) {
+			this.feet = feet;
+		}
+
+
+		@Override
+		public boolean equals(Object obj) {
+			// Same object reference
+			if(this == obj) return true;
+			 
+			// Null Pointer
+			if(obj == null || this.getClass() != obj.getClass()) {
+				return false;
+			}
+			
+			// Compare double values safety
+			return Double.compare(this.feet, ((Feet)obj).feet) == 0; 
+		}
+	}
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter 2 feet: ");
+		
+		double input1 = sc.nextDouble();
+		double input2 = sc.nextDouble();
+		
+		Feet feet1 = new Feet(input1);
+		Feet feet2 = new Feet(input2);
+		
+		System.out.println("Equal (" + feet1.equals(feet2) + ")"); 
 	}
 }
--- src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementAppTest.java
@@ -1,8 +1,42 @@
 package com.apps.quantitymeasurementapp;
 
-public class QuantityMeasurementAppTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-	public static void main(String[] args) {
-		
+import org.junit.jupiter.api.Test;
+
+import com.apps.quantitymeasurementapp.QuantityMeasurementApp.Feet;
+
+public class QuantityMeasurementAppTest {
+	
+	Feet feet1 = new Feet(1.0);
+	Feet feet2 = new Feet(1.0);
+	Feet feet3 = new Feet(2.0);
+	
+	
+	@Test
+	public void testFeetEquality_sameValue() {
+		assertEquals(true, feet1.equals(feet2));
+	}
+	
+	@Test
+	public void estEquality_DifferentValue() {
+		assertEquals(false, feet2.equals(feet3));;
+	}
+	
+	@Test
+	public void testEquality_NullComparision() {
+		assertEquals(false, feet1.equals(null)); 
+	}
+	
+	@Test
+	public void testEquality_NonNumericInput() {
+		String non_numeric = "3.0";
+	
+		assertFalse(feet1.equals(non_numeric));
+	}
+	
+	@Test
+	public void testEquality_SameReference() {
+		assertTrue(feet1.equals(feet1));
 	}
 }
