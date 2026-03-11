4780f7a94c6afbfb714ff3acda45f1de5dea2e87
Avinash kumar <avinashkumar40977@gmail.com>
2026-03-12 04:56:25 +0530

    [AVINASH] : Added differents types of unit enum in UC15

---
+++ src/main/java/com/apps/quantitymeasurementapp/unit/IMeasurable.java
@@ -0,0 +1,18 @@
+package com.apps.quantitymeasurementapp.unit;
+
+import com.apps.quantitymeasurementapp.unit.IMeasurable;
+
+public interface IMeasurable { 
+	public String getUnitName();
+	
+	public double getConversionFactor();
+	
+	public double convertToBaseUnit(double value);
+	 
+	public double convertFromBaseUnit(double baseValue);
+
+	public String getMeasurementType();
+	
+	public IMeasurable getUnitInstance(String unitName);
+	 
+}
