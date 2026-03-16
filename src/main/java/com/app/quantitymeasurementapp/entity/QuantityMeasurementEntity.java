8a93ce750d64fe3e52db9fcc7204629a5987afa5
Avinash kumar <avinashkumar40977@gmail.com>
2026-03-16 09:51:02 +0530

    [AVINASH] : Updated the QuantityMeasurementEntity class code

--- src/main/java/com/app/quantitymeasurementapp/entity/QuantityMeasurementEntity.java
+++ src/main/java/com/app/quantitymeasurementapp/entity/QuantityMeasurementEntity.java
@@ -1,8 +1,7 @@
 package com.app.quantitymeasurementapp.entity;
 
 import java.io.Serializable;
-
-import com.app.quantitymeasurementapp.unit.IMeasurable;
+import java.util.Objects;
 
 public class QuantityMeasurementEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -24,70 +23,107 @@ public class QuantityMeasurementEntity implements Serializable {
 	public String errorMessage;
 	public boolean isError;
 	
-	 public QuantityModel<IMeasurable> thisQuantity;
-	 public QuantityModel<IMeasurable> thatQuantity;
 	
-	
-	// 3
-	public QuantityMeasurementEntity(QuantityModel<IMeasurable> thisQuantity, QuantityModel<IMeasurable> thatQuantity, String operation) {
-		this.thisQuantity = thisQuantity;
-		this.thatQuantity = thatQuantity;
-		this.operation = operation;
-		
-		
-	}
-	
-	// Single Operand 4
-	public QuantityMeasurementEntity(QuantityModel<IMeasurable> thisQuantity, QuantityModel<IMeasurable> thatQuantity, String operation, String result) { 
-		this(thisQuantity, thatQuantity, operation);
-		this.resultString = result;
-	}
-	
-	// Single Operand 4
-	public QuantityMeasurementEntity(QuantityModel<IMeasurable> thisQuantity, QuantityModel<IMeasurable> thatQuantity, String operation, double resultValue) { 
-		this(thisQuantity, thatQuantity, operation);
-		this.resultValue = resultValue;
-	}
-	 
-	// 4
-	public QuantityMeasurementEntity(QuantityModel<IMeasurable> thisQuantity, QuantityModel<IMeasurable> thatQuantity, String operation, QuantityModel<IMeasurable> result) {
-		this(thisQuantity, thatQuantity, operation);
-		this.resultValue = result.getValue();
-		this.resultUnit = result.getUnit().getUnitName();
-		this.resultMeasurementType = result.getUnit().getMeasurementType();
-	}
-	
-	// 5
-	public QuantityMeasurementEntity(QuantityModel<IMeasurable> thisQuantity, QuantityModel<IMeasurable> thatQuantity, String operation, String errorMessage, boolean isError) {
-		this(thisQuantity, thatQuantity, operation);
-		this.errorMessage = errorMessage;
-		this.isError = isError;
-	}
+    public QuantityMeasurementEntity(double thisValue, String thisUnit, String thisMeasurementType, double thatValue, String thatUnit, String thatMeasurementType, String operation, String resultString) {
+        initializeCommonFields(thisValue, thisUnit, thisMeasurementType, thatValue, thatUnit, thatMeasurementType, operation);
+
+        this.resultString = resultString;
+        this.isError = false;
+    }
+    
+    public QuantityMeasurementEntity(double thisValue, String thisUnit, String thisMeasurementType, double thatValue, String thatUnit, String thatMeasurementType, String operation, double resultValue, String resultUnit, String resultMeasurementType) {
+        initializeCommonFields(thisValue, thisUnit, thisMeasurementType, thatValue, thatUnit, thatMeasurementType, operation);
+
+        this.resultValue = resultValue;
+        this.resultUnit = resultUnit;
+        this.resultMeasurementType = resultMeasurementType;
+        this.isError = false;
+    }
+    
+    public QuantityMeasurementEntity(double thisValue, String thisUnit, String thisMeasurementType, double thatValue, String thatUnit, String thatMeasurementType, String operation, double resultValue, String resultUnit, String resultMeasurementType, String resultString, boolean isError, String errorMessage) {
+        initializeCommonFields(thisValue, thisUnit, thisMeasurementType, thatValue, thatUnit, thatMeasurementType, operation);
+        this.resultValue = resultValue;
+        this.resultUnit = resultUnit;
+        this.resultMeasurementType = resultMeasurementType;
+
+        this.resultString = resultString;
+
+        this.isError = isError;
+        this.errorMessage = errorMessage;
+    }
+    
+    public QuantityMeasurementEntity(double thisValue, String thisUnit, String thisMeasurementType, double thatValue, String thatUnit, String thatMeasurementType, String operation, double resultValue) {
+        initializeCommonFields(thisValue, thisUnit, thisMeasurementType, thatValue, thatUnit, thatMeasurementType, operation);
+
+        this.resultValue = resultValue;
+        this.isError = false;
+    }
+    
+    public QuantityMeasurementEntity(double thisValue, String thisUnit, String thisMeasurementType, double thatValue, String thatUnit, String thatMeasurementType, String operation, String errorMessage, boolean isError) {
+        initializeCommonFields(thisValue, thisUnit, thisMeasurementType, thatValue, thatUnit, thatMeasurementType, operation);
+
+        this.errorMessage = errorMessage;
+        this.isError = isError;
+    }
+    
+    private void initializeCommonFields(double thisValue, String thisUnit, String thisMeasurementType, double thatValue, String thatUnit, String thatMeasurementType, String operation) {
+        this.thisValue = thisValue;
+        this.thisUnit = thisUnit;
+        this.thisMeasurementType = thisMeasurementType;
+
+        this.thatValue = thatValue;
+        this.thatUnit = thatUnit;
+        this.thatMeasurementType = thatMeasurementType;
+
+        this.operation = operation;
+    }
 	
 	
 	@Override
 	public boolean equals(Object obj) {
-		if(this == obj) return true;
-		
-		if(obj == null || obj.getClass() != this.getClass()) {
-			return false;
-		}
-		
-		QuantityModel<?> thatQuantity = (QuantityModel<?>) obj; 
-		
-		 if(!this.thisUnit.getClass().equals(thatQuantity.getUnit().getClass())) {
-		     return false;
-		 }
-		
-		double thisBase = thisQuantity.unit.convertToBaseUnit(thisValue);
-		double thatBase = thatQuantity.unit.convertToBaseUnit(thatQuantity.value); 
-		 
-		return Math.abs(thisBase - thatBase) < EPSILON; 
+		if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false; 
+
+        QuantityMeasurementEntity that = (QuantityMeasurementEntity) obj;
+        return Double.compare(that.thisValue, thisValue) == 0 &&
+                Double.compare(that.thatValue, thatValue) == 0 &&
+                Double.compare(that.resultValue, resultValue) == 0 &&
+                isError == that.isError &&
+                Objects.equals(thisUnit, that.thisUnit) &&
+                Objects.equals(thisMeasurementType, that.thisMeasurementType) &&
+                Objects.equals(thatUnit, that.thatUnit) &&
+                Objects.equals(thatMeasurementType, that.thatMeasurementType) &&
+                Objects.equals(operation, that.operation) &&
+                Objects.equals(resultUnit, that.resultUnit) &&
+                Objects.equals(resultMeasurementType, that.resultMeasurementType) &&
+                Objects.equals(resultString, that.resultString) &&
+                Objects.equals(errorMessage, that.errorMessage); 
 	}
-	
+	 
 	@Override
-	public String toString() {
-		return thisValue + " " + thisUnit; 
+	public String toString() { 
+		 return "QuantityMeasurementEntity Details:\n" +
+	                "----------------------------------\n" +
+	                "This Value            : " + thisValue + "\n" +
+	                "This Unit             : " + thisUnit + "\n" +
+	                "This Measurement Type : " + thisMeasurementType + "\n" +
+	                "\n" +
+	                "That Value            : " + thatValue + "\n" +
+	                "That Unit             : " + thatUnit + "\n" +
+	                "That Measurement Type : " + thatMeasurementType + "\n" +
+	                "\n" +
+	                "Operation             : " + operation + "\n" +
+	                "\n" +
+	                "Result Value          : " + resultValue + "\n" +
+	                "Result Unit           : " + resultUnit + "\n" +
+	                "Result MeasurementType: " + resultMeasurementType + "\n" +
+	                "Result String         : " + resultString + "\n" +
+	                "\n" +
+	                "Error                 : " + isError + "\n" +
+	                "Error Message         : " + errorMessage + "\n";
 	}
 	
 	
