867e83b0a86594297155c10ebda586aed6328fa3
Avinash kumar <avinashkumar40977@gmail.com>
2026-03-16 09:50:17 +0530

    [AVINASH] : Updated the QuantityMeasurementApp code for Addition, subtraction, and division

--- src/main/java/com/app/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/app/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -5,26 +5,25 @@ import java.util.logging.Logger;
 import com.app.quantitymeasurementapp.controller.QuantityMeasurementController;
 import com.app.quantitymeasurementapp.entity.QuantityDTO;
 import com.app.quantitymeasurementapp.repository.IQuantityMeasurementRepository;
-import com.app.quantitymeasurementapp.repository.QuantityMeasurementCacheRepository;
+import com.app.quantitymeasurementapp.repository.QuantityMeasurementDatabaseRepository;
 import com.app.quantitymeasurementapp.service.QuantityMeasurementServiceImpl;
-import com.app.quantitymeasurementapp.unit.LengthUnit;
 
 public class QuantityMeasurementApp {
 	
 	// Singleton instance
 	private static QuantityMeasurementApp instance;
 	
-	public QuantityMeasurementController controller;
-	public IQuantityMeasurementRepository repository;
-	public QuantityMeasurementServiceImpl service;
+	private QuantityMeasurementController controller;
+	private IQuantityMeasurementRepository repository;
+	private QuantityMeasurementServiceImpl service;
 	
 	private static final Logger logger = Logger.getLogger(QuantityMeasurementApp.class.getName());
 
 	private QuantityMeasurementApp() {
-		this.repository = QuantityMeasurementCacheRepository.getInstance();
+		this.repository = QuantityMeasurementDatabaseRepository.getInstance();
 		this.service = new QuantityMeasurementServiceImpl(this.repository);
 		this.controller = new QuantityMeasurementController(service);
-		logger.info("Quantity Measurement Application initialized with " + "Cache Repository");
+		logger.info("Quantity Measurement Application initialized with Database Repository");
 	}
 		
 	// Return a single entity of QuantityMeasurementApp
@@ -37,14 +36,51 @@ public class QuantityMeasurementApp {
 		
 	
 	public static void main(String[] args) {
-		
 		QuantityMeasurementController controller = getInstance().controller;
-		
+				
 		// Testing Comparisons of Two QuantityDTOs
-		QuantityDTO q1 = new QuantityDTO(2.0, LengthUnit.FEET.getUnitName(), LengthUnit.FEET.getMeasurementType());
-		QuantityDTO q2 = new QuantityDTO(24.0, LengthUnit.INCHES.getUnitName(), LengthUnit.INCHES.getMeasurementType());
+		QuantityDTO q1 = new QuantityDTO(2.0, "FEET", "LengthUnit");
+		QuantityDTO q2 = new QuantityDTO(24.0, "INCHES", "LengthUnit");
 		
 		boolean comparisonResult = controller.performComparison(q1, q2);
-		logger.info("Comparison result: " + comparisonResult);
+		logger.info(comparisonResult + " (1 Foot = 12 Inches)");
+				
+		
+		// ---------------- Addition ----------------
+	    QuantityDTO addQ1 = new QuantityDTO(2.0, "FEET", "LengthUnit");
+	    QuantityDTO addQ2 = new QuantityDTO(24.0, "INCHES", "LengthUnit");
+
+	    QuantityDTO additionResult = controller.performAddition(addQ1, addQ2);
+	    logger.info("Addition Result: " + additionResult);
+
+	    
+	    // Addition with target unit
+	    QuantityDTO additionTarget = new QuantityDTO(0.0, "INCHES", "LengthUnit");
+	    
+	    QuantityDTO additionWithTargetResult = controller.performAddition(addQ1, addQ2, additionTarget);
+	    logger.info("Addition Result in Target Unit: " + additionWithTargetResult);
+
+	    
+	    // ---------------- Subtraction ----------------
+	    QuantityDTO subQ1 = new QuantityDTO(5.0, "FEET", "LengthUnit");
+	    QuantityDTO subQ2 = new QuantityDTO(12.0, "INCHES", "LengthUnit");
+
+	    QuantityDTO subtractionResult = controller.performSubtraction(subQ1, subQ2);
+	    logger.info("Subtraction Result: " + subtractionResult);
+
+	    
+	    // Subtraction with target unit
+	    QuantityDTO subtractionTarget = new QuantityDTO(0.0, "INCHES", "LengthUnit");
+	   
+	    QuantityDTO subtractionWithTargetResult = controller.performSubtraction(subQ1, subQ2, subtractionTarget);
+	    logger.info("Subtraction Result in Target Unit: " + subtractionWithTargetResult);
+
+	    
+	    // ---------------- Division ----------------
+	    QuantityDTO divQ1 = new QuantityDTO(24.0, "INCHES", "LengthUnit");
+	    QuantityDTO divQ2 = new QuantityDTO(12.0, "INCHES", "LengthUnit");
+
+	    QuantityDTO divisionResult = controller.performDivision(divQ1, divQ2);
+	    logger.info("Division Result: " + divisionResult);
 	}
 }
