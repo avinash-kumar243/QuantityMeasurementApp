1015038958dbe2579de5779f2a2e5ed5199598a9
Avinash kumar <avinashkumar40977@gmail.com>
2026-03-16 09:51:46 +0530

    [AVINASH] : Updated the QuantityMeasurementException code

--- src/main/java/com/app/quantitymeasurementapp/exception/QuantityMeasurementException.java
+++ src/main/java/com/app/quantitymeasurementapp/exception/QuantityMeasurementException.java
@@ -4,4 +4,7 @@ public class QuantityMeasurementException extends RuntimeException {
 	public QuantityMeasurementException(String message) {
 		super(message);
 	}
+	public QuantityMeasurementException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
