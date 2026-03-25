38b654c7cb4eb5d689d511bd408a1b7abe7590f7
Avinash kumar <avinashkumar40977@gmail.com>
2026-03-26 02:28:36 +0530

    [AVINASH] : Updated the SecurityConfig class with main security rules

--- src/main/java/com/app/quantitymeasurementapp/config/SecurityConfig.java
+++ src/main/java/com/app/quantitymeasurementapp/config/SecurityConfig.java
@@ -7,25 +7,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import lombok.AllArgsConstructor;
+
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@AllArgsConstructor
+public class SecurityConfig {  
+	
+//	This class is the main Spring Security setup class.
+	
+//	It tells our application:
+//
+//		which URLs are public
+//		which URLs need JWT token
+//		how to handle unauthorized access
+//		where to run our JWTFilter
+//		how CORS should work
+//		session should be stateless
+	
+	
+	private JWTFilter filter;
+	private AuthenticationEntryPoint point; 
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-			.csrf(csrf -> csrf.disable())
-			.sessionManagement(Session  -> Session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authorizeHttpRequests(authz -> authz.anyRequest().permitAll()
-		);
+		 	.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authz -> authz
+					.requestMatchers("/auth/**").permitAll()
+					.requestMatchers("/h2-console/**").permitAll()
+	                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+					.anyRequest().authenticated())
+					.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.exceptionHandling(ex -> ex.authenticationEntryPoint(point));
+			
 		
-		// For H2 Console
-		http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+			http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+			http.headers(headers-> headers.frameOptions(frame-> frame.disable()));
+		
 		return http.build();
 	}
 	
