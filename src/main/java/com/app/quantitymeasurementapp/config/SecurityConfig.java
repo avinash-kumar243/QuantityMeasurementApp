4d533bd23b14210dff2bacf20bffdd22d2671137
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-01 16:59:36 +0530

    [AVINASH] : Updated config while connecting Backend with Frontend

--- src/main/java/com/app/quantitymeasurementapp/config/SecurityConfig.java
+++ src/main/java/com/app/quantitymeasurementapp/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {	
 		CorsConfiguration configuration = new CorsConfiguration();
 		
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:5500","http://localhost:4200"));
 		
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		
