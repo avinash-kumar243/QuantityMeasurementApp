efc0a5ea8d02e2408f1afd077d3fd0029bcdfb8e
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-02 17:24:21 +0530

    [AVINASH] : Added the microservice layer for repository layer in quantity-service

---
+++ microservices/quantity-service/src/main/java/com/app/microservices/quantity_service/repository/QuantityMeasurementRepository.java
@@ -0,0 +1,31 @@
+package com.app.microservices.quantity_service.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.app.microservices.quantity_service.entity.QuantityMeasurementEntity;
+
+
+@Repository
+public interface QuantityMeasurementRepository extends JpaRepository<QuantityMeasurementEntity, Long>{
+	
+	List<QuantityMeasurementEntity> findByOperation(String operation);
+	
+	List<QuantityMeasurementEntity> findByThisMeasurementType(String measurementType); 
+	
+	List<QuantityMeasurementEntity> findByCreatedAtAfter(LocalDateTime date);
+	
+	@Query("SELECT e FROM QuantityMeasurementEntity e WHERE e.operation = :operation " + "AND e.isError = false")
+	List<QuantityMeasurementEntity> findSuccessfulOperations(@Param("operation") String operation);
+	
+	long countByOperationAndIsErrorFalse(String operation);
+	
+	List<QuantityMeasurementEntity> findByIsErrorTrue();
+	
+	List<QuantityMeasurementEntity> findAllByOrderByCreatedAtDesc();
+}
