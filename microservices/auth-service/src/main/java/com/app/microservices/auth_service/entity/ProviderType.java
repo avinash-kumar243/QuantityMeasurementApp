bd86c42759986dd498897216415886852dd059f0
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-03 10:07:16 +0530

    [AVINASH] : Added microservice layer and auth-service entities in quantity measurement app

---
+++ microservices/auth-service/src/main/java/com/app/microservices/auth_service/entity/ProviderType.java
@@ -0,0 +1,6 @@
+package com.app.microservices.auth_service.entity;
+
+public enum ProviderType {
+	LOCAL,  // Our app should know that the user signup normally or user signup from Google
+	GOOGLE
+}
