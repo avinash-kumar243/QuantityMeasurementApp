e8f6c83fa4978b19bd3e1ae518155401500dacd7
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-03 10:05:36 +0530

    [AVINASH] : Added microservice layer and auth-service configuration in quantity measurement app

---
+++ microservices/auth-service/src/main/java/com/app/microservices/auth_service/config/JWTFilter.java
@@ -0,0 +1,76 @@
+package com.app.microservices.auth_service.config;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.app.microservices.auth_service.service.CustomUserDetailsService;
+import com.app.microservices.auth_service.service.JWTService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {  // JWTFilter = It ask JWT token (from JWTSevice) for every request, and authenticate user
+	 
+	private CustomUserDetailsService customUserDetailsService;
+	private JWTService jwtService;
+	
+	// Constructor Injection
+	public JWTFilter(CustomUserDetailsService customUserDetailsService, JWTService jwtService) {
+		this.customUserDetailsService = customUserDetailsService;
+		this.jwtService = jwtService;
+	}
+	
+	
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+	    String path = request.getServletPath();
+
+	    return path.startsWith("/auth/")
+	            || path.startsWith("/oauth2/")
+	            || path.startsWith("/login/")
+	            || path.startsWith("/h2-console/")
+	            || path.startsWith("/swagger-ui/")
+	            || path.startsWith("/v3/api-docs");
+	}
+
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+		String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String email = null;
+        
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+
+            try {
+            	email = jwtService.extractEmailFromToken(token);
+            } catch (Exception e) {
+                System.out.println("Invalid JWT token: " + e.getMessage());
+            }
+        }
+        
+        if(email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails = customUserDetailsService.loadUserByUsername(email);
+
+            if(jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        
+        filterChain.doFilter(request, response);
+    }
+	
+}
